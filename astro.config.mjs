10c598ef9ef7484eae74d9e7b216e0569ff0d230
adrianadonca1999-ai <adrianadonca1999@gmail.com>
2026-06-02 20:27:20 -0700

    Add custom domain adrianadonca.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://adrianadonca1999-ai.github.io',
-  base: '/my-portfolio',
+  site: 'https://adrianadonca.com',
 });
