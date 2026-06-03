ef3fb54ecbb15ddc5ef5a76fa0ce65b8428c3277
adrianadonca1999-ai <adrianadonca1999@gmail.com>
2026-06-02 19:26:31 -0700

    Initial portfolio site

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://adrianadonca1999-ai.github.io',
+  base: '/my-portfolio',
+});
